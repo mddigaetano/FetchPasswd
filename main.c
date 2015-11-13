a10b7a58a9a0b4ea7185acc4a549c1aea67beeb4
amedeo <amedeo@amedeo-VirtualBox>
2015-11-13 15:58:21 +0100

    Aggiunti commenti

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PATH_PASSWD "/etc/passwd"
-#define MAX_STRLEN 30
+#define PATH_PASSWD "/etc/passwd"                                               //percorso file passwd;
+#define MAX_STRLEN 30                                                           //lunghezza massima per nome;
 
 int main(int argc, char** argv) {
-    FILE* passwd;
-    char name[MAX_STRLEN];
-    char trash[100];
-    int uid;
+    FILE* passwd;                                                               //puntatore a file passwd;
+    char name[MAX_STRLEN];                                                      //stringa contenente i nomi;
+    char trash[100];                                                            //conterrà le informazioni non necessarie al compito;
+    int uid;                                                                    //conterrà l'uid dell'utente corrente;
     
-    if(!(passwd = fopen(PATH_PASSWD,"r")))
-        exit(1);
+    if(!(passwd = fopen(PATH_PASSWD,"r")))                                      //controllo apertura file passwd;
+        exit(1);                                                                // ERROR N°1 || failed to open passwd;
     
     printf("Ecco la lista di utenti umani:\n\n");
     
     do{
-        fscanf(passwd,"%[^:]:x:%d:%[^\n]\n", name, &uid, trash);
-        if(uid >= 1000 && uid!=65534)
-            printf("%s\n",name);
-    }while(!(feof(passwd)));
+        fscanf(passwd,"%[^:]:x:%d:%[^\n]\n", name, &uid, trash);                //estrazione dati da singola riga di passwd;
+        if(uid >= 1000 && uid<=9999)                                            //range di UID possibili per utenti umani;
+            printf("%s\n",name);                                                //stampa nome utente;
+    }while(!(feof(passwd)));                                                    //controllo fine file;
 
     return (EXIT_SUCCESS);
 }
